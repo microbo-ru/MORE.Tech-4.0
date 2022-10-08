8b28f204ec8c83c3745da9f6a7439a9dcb94899a
Andrey Egorov <a-n-egorov@yandex.ru>
2022-10-09 00:33:41 +0300

    Added inset publuc and private keys from ui

--- web-app/src/user/profile/Profile.js
+++ web-app/src/user/profile/Profile.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
+import { wallet } from '../../util/APIUtils';
 import './Profile.css';
+import Alert from 'react-s-alert';
 
 class Profile extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            walletPublicKey: this.props.currentUser.walletPublicKey,
+            walletPrivateKey: this.props.currentUser.walletPrivateKey
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         console.log(props);
     }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;        
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName] : inputValue
+        });        
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();   
+
+        const walletRequest = Object.assign({}, this.state);
+
+        wallet(walletRequest)
+        .then(response => {
+            Alert.success("You're successfully registered. Please login to continue!");
+        }).catch(error => {
+            Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');            
+        });
+    }
+
     render() {
         return (
             <div className="profile-container">
                 <div className="container">
                     <div className="profile-info">
+                        
+                        <div className="signup-content">
                         <div className="profile-avatar">
                             { 
                                 this.props.currentUser.imageUrl ? (
@@ -25,6 +61,23 @@ class Profile extends Component {
                         <div className="profile-name">
                            <h2>{this.props.currentUser.name}</h2>
                            <p className="profile-email">{this.props.currentUser.email}</p>
+                        </div>
+                        
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-item">
+                            <input type="walletPublicKey" name="walletPublicKey" 
+                                className="form-control" placeholder="walletPublicKey"
+                                value={this.state.walletPublicKey} onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-item">
+                            <input type="walletPrivateKey" name="walletPrivateKey" 
+                                className="form-control" placeholder="walletPrivateKey"
+                                value={this.state.walletPrivateKey} onChange={this.handleInputChange}/>
+                            </div>
+                            <div className="form-item">
+                            <button type="submit" className="btn btn-block btn-primary" >Update</button>
+                            </div>
+                        </form>
                         </div>
                     </div>
                 </div>    
