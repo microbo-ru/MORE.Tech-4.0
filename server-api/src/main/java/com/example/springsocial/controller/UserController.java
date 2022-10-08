6f89d2d3d50dc1f2ed73ded420537fa64bf06bac
adavydenko <alexandrdavydenko@yandex.ru>
2022-10-09 01:47:15 +0300

    Statistic support added + repositories list

--- server-api/src/main/java/com/example/springsocial/controller/UserController.java
+++ server-api/src/main/java/com/example/springsocial/controller/UserController.java
@@ -2,13 +2,25 @@ package com.example.springsocial.controller;
 
 import com.example.springsocial.exception.ResourceNotFoundException;
 import com.example.springsocial.model.User;
+import com.example.springsocial.payload.ApiResponse;
+import com.example.springsocial.payload.AuthResponse;
+import com.example.springsocial.payload.LoginRequest;
+import com.example.springsocial.payload.WalletUpdateRequest;
 import com.example.springsocial.repository.UserRepository;
 import com.example.springsocial.security.CurrentUser;
 import com.example.springsocial.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -21,5 +33,19 @@ public class UserController {
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+
+    @PostMapping("/user/wallet")
+    public ResponseEntity<?> postWallet(@CurrentUser UserPrincipal userPrincipal, @Valid @RequestBody WalletUpdateRequest walletRequest) {
+
+        User user = userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+
+        user.setWalletPrivateKey(walletRequest.getWalletPrivateKey());
+        user.setWalletPublicKey(walletRequest.getWalletPublicKey());
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new ApiResponse(true, "Wallet properties are saved." ));
     }
 }
